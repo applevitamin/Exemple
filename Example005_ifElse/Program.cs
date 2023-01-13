9f3ad585f25d7b7142ab6ef0706165ac855209ca
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-13 14:19:36 +0300

    Сделали приветствие Маши

--- Example005_ifElse/Program.cs
+++ Example005_ifElse/Program.cs
@@ -1,2 +1,12 @@
 ﻿Console.Write("Введите имя пользователя: ");
 string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура Маша! С возвращением, рады тебя видеть! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
