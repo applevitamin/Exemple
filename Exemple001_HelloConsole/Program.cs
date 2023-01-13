5f3b55b2c5466404d5a77daaa0579f68da5778be
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-13 13:17:39 +0300

    add Summ

--- Exemple001_HelloConsole/Program.cs
+++ Exemple001_HelloConsole/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("2 Hello, World!");
-//dotnet new console создать новый проект
+﻿//dotnet new console создать новый проект
 //dot run запуск проекта
 //Console обращение к терминалу
+Console.WriteLine("2 Hello, World!");
--- Exemple002_HelloUser/Program.cs
+++ Exemple002_HelloUser/Program.cs
@@ -2,3 +2,6 @@
 string username = Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(username);
+//Write(); вывод в одну строку
+//WriteLine(); В конце перейти на новую строку
+//ReadLine(); Считать строку из терминала
