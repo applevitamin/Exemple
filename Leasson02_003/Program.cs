c4c16f3f19ebc53c6e73043e44998093281fdf16
Vitalyi <119963502+applevitamin@users.noreply.github.com>
2023-01-15 20:33:07 +0300

    add Leasson02_003

---
+++ Leasson02_003/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 3, 18, 54, 65, 18, 9 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //если значение индекса совпадает выведет первый и остановится.
+    }
+index++;
+}
